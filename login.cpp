1ceff850e91ca4226461e70844235055c741e7c5
Mr. Anderson <as27041016@gmail.com>
2026-04-01 18:00:07 -0400

    Refactor: solved split by zero of the login, add variables to the struct, optimization of the function convertion.

--- login.cpp
+++ login.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
-bool verifInicioSesion(std::string usuarioCorrecto, int claveCorrecta);
+bool verifInicioSesion(struct entradas &entrada);
+void transferencia (double &ctaDesc, double &ctaAum, double monto);
+void conversion(double ctaCteBs, double ctaAroBs, double &ctaCteDs, double &ctaAroDs, double tasa);
+void resta(double &cuenta, double monto, double &factura);
 struct cuentas{
     double corriente = 5000.0; 
     double ahorro = 1000; 
     double corrienteDs;
     double ahorroDs; 
-    double tasa = 480;
+    double tasa = 0;
 };
 struct facturas{
     double electricidad = 100;
@@ -15,26 +18,26 @@ struct facturas{
 };
 struct entradas{
     double monto;
+    int opcMenPri = 1;
+    int opcMenSal = 01;
+    int opcMenTra = 0;
+    int opcMenSer = 0;
+    int opcMenSerIf = 0;
+    int tipDeCue = 0;
+    std::string usuarioCto = "a";
+    int claveCto = 1;
+    std::string usuarioInput;
+    int claveInput;
 };
-void transferencia (double &ctaDesc, double &ctaAum, double monto);
-void conversion(double &ctaCteBs, double &ctaAroBs, double &ctaCteDs, double &ctaAroDs, double tasa);
-void resta(double &cuenta, double monto, double &factura);
 int main() {
     cuentas miCuenta;
     entradas miEntrada;
     facturas miFactura;
-    std::string usuarioCorrecto = "usuario";
-    int claveCorrecta = 123456;
-    int intentoClave;
+    miEntrada.usuarioCto = "a";
+    miEntrada.claveCto = 1;
     std::string intentoUsuario;
-    int opcionMenu = 1;
-    int opcionMenuSaldo;
-    int opcionMenuTransf;
-    int opcionMenuServicio;
-    int opcionMenuServicioIf;
-    int tipoDeCuenta;
-        if (verifInicioSesion(usuarioCorrecto, claveCorrecta)) {
-            while(opcionMenu != 0) {
+        if (verifInicioSesion(miEntrada)) {
+            while(miEntrada.opcMenPri != 0) {
                 std::cout <<"**MENU PRINCIPAL**" << std::endl;
                 std::cout << std::endl;
                 std::cout <<"#1 Consulta de saldo" << std::endl;
@@ -43,9 +46,9 @@ int main() {
                 std::cout <<"#0 Salir del programa" << std::endl;
                 std::cout << std::endl;                
                 std::cout << "INGRESA EL NUMERO DE LA OPCION ELEGIDA: ";
-                std::cin >> opcionMenu;
+                std::cin >> miEntrada.opcMenPri;
                 std::cout << std::endl;
-                switch(opcionMenu){
+                switch(miEntrada.opcMenPri){
                     case 1:
                         std::cout << "**CONSULTA DE SALDO**" << std::endl;
                         std::cout << std::endl;
@@ -57,10 +60,10 @@ int main() {
                         std::cout << "#1 Equivalencia en dolares" << std::endl;
                         std::cout << "#0 Volver al menu principal" << std::endl;
                         std::cout << std::endl;
-                        std::cin >> opcionMenuSaldo;
+                        std::cin >> miEntrada.opcMenSal;
                         std::cout << std::endl;
-                        while(opcionMenuSaldo != 0) {
-                            if(opcionMenuSaldo == 1){
+                        while(miEntrada.opcMenSal != 0) {
+                            if(miEntrada.opcMenSal == 1 && miCuenta.tasa > 0){
                                 conversion(miCuenta.corriente, miCuenta.ahorro, miCuenta.corrienteDs, miCuenta.ahorroDs, miCuenta.tasa);
                                 std::cout << "**CONSULTA DE SALDO**" << std::endl;
                                 std::cout << std::endl;
@@ -74,9 +77,9 @@ int main() {
                                 std::cout << "#2 Volver a saldo en bolivares" << std::endl;
                                 std::cout << "#0 Volver al menu principal" << std::endl;
                                 std::cout << std::endl;
-                                std::cin >> opcionMenuSaldo;
+                                std::cin >> miEntrada.opcMenSal;
                                 std::cout << std::endl;
-                                while (opcionMenuSaldo == 2){
+                                while (miEntrada.opcMenSal == 2){
                                     std::cout << "**CONSULTA DE SALDO**" << std::endl;
                                     std::cout << std::endl;
                                     std::cout << "Cuenta corriente: BS " << miCuenta.corriente << std::endl;
@@ -87,13 +90,17 @@ int main() {
                                     std::cout << "#1 Equivalencia en dolares" << std::endl;
                                     std::cout << "#0 Volver al menu principal" << std::endl;
                                     std::cout << std::endl;
-                                    std::cin >> opcionMenuSaldo;
+                                    std::cin >> miEntrada.opcMenSal;
                                     std::cout << std::endl;                             
                                 }
-                            } else if(opcionMenuSaldo == 2){
-                                opcionMenu = opcionMenuSaldo;
+                            } else if (miEntrada.opcMenSal == 1 && miCuenta.tasa <= 0){
+                                std::cout << "No se puede dividir por cero" << std::endl;
+                                std::cout << std::endl;
+                                miEntrada.opcMenSal = 0;
+                            } else if(miEntrada.opcMenSal == 2){
+                                miEntrada.opcMenPri = miEntrada.opcMenSal;
                             } else {
-                                opcionMenu = opcionMenuSaldo;
+                                miEntrada.opcMenPri = miEntrada.opcMenSal;
                             }
                         }
                     break;
@@ -104,9 +111,9 @@ int main() {
                         std::cout << "#2 Cuenta de ahorro a cuenta de corriente" << std::endl; 
                         std::cout << std::endl;
                         std::cout << "INGRESE EL NUMERO DE LA OPCION ELEGIDA: "; 
-                        std::cin >> opcionMenuTransf;
+                        std::cin >> miEntrada.opcMenTra;
                         std::cout << std::endl;
-                        if (opcionMenuTransf == 1) {
+                        if (miEntrada.opcMenTra == 1) {
                             std::cout << "**CUENTA CORRIENTE A CUENTA DE AHORRO**" << std::endl;
                             std::cout << std::endl;
                             std::cout << "INGRESE MONTO: ";
@@ -121,9 +128,9 @@ int main() {
                             std::cout << "*El saldo de su cuenta de ahorro es de: BS " << miCuenta.ahorro << std::endl;
                             std::cout << std::endl;
                             std::cout << "INGRESE CUALQUIER NUMERO PARA VOLVER AL MENU PRINCIPAL: ";
-                            std::cin >> opcionMenuTransf;
+                            std::cin >> miEntrada.opcMenTra;
                             std::cout << std::endl;
-                        } else if(opcionMenuTransf == 2){
+                        } else if(miEntrada.opcMenTra == 2){
                             std::cout << "**CUENTA DE AHORRO A CUENTA CORRIENTE**" << std::endl;
                             std::cout << std::endl;
                             std::cout << "INGRESE MONTO: ";
@@ -138,12 +145,12 @@ int main() {
                             std::cout << "*El saldo de su cuenta de ahorro es de: BS " << miCuenta.ahorro << std::endl;
                             std::cout << std::endl;
                             std::cout << "INGRESE CUALQUIER NUMERO PARA VOLVER AL MENU PRINCIPAL: ";
-                            std::cin >> opcionMenuTransf;
+                            std::cin >> miEntrada.opcMenTra;
                         }
                         break;
                     case 3:
-                        opcionMenuServicio = opcionMenu;
-                        while (opcionMenuServicio != 0) {
+                        miEntrada.opcMenSer = miEntrada.opcMenPri;
+                        while (miEntrada.opcMenSer != 0) {
                             std::cout << "**PAGOS DE SERVICIOS**" << std::endl;
                             std::cout << std::endl;
                             std::cout << "#1 Electricidad" << std::endl;
@@ -152,9 +159,9 @@ int main() {
                             std::cout << "#0 Salir al menu principal" << std::endl; 
                             std::cout << std::endl;
                             std::cout << "INGRESE EL NUMERO DEL SERVICIO QUE DESEA PAGAR: "; 
-                            std::cin >> opcionMenuServicio;
+                            std::cin >> miEntrada.opcMenSer;
                             std::cout << std::endl;
-                            if (opcionMenuServicio == 1) {
+                            if (miEntrada.opcMenSer == 1) {
                                 std::cout << std::endl;
                                 std::cout << "**ELECTRICIDAD**" << std::endl;
                                 std::cout << std::endl;
@@ -166,12 +173,12 @@ int main() {
                                 std::cout << "#2 Cuenta ahorro" <<std::endl;
                                 std::cout << std::endl;
                                 std::cout << "INGRESE LA OPCION DEL TIPO DE CUENTA PARA EL PAGO DE SERVICIO: ";
-                                std::cin >> tipoDeCuenta;
+                                std::cin >> miEntrada.tipDeCue;
                                 std::cout << std::endl;
                                 std::cout << "INGRESE MONTO: ";
                                 std::cin >> miEntrada.monto;
                                 std::cout << std::endl;
-                                if (tipoDeCuenta == 1) {
+                                if (miEntrada.tipDeCue == 1) {
                                     resta(miCuenta.corriente, miEntrada.monto, miFactura.electricidad);
                                     std::cout << "*OPERACION EXITOSA*" <<std::endl;
                                     std::cout << std::endl;
@@ -180,7 +187,7 @@ int main() {
                                     std::cout << "*La deuda pendiente con respecto al servicio es de: BS " << miFactura.electricidad << std::endl;
                                     std::cout << std::endl;
                                     std::cout << "*El saldo actual de su cuenta corriente posterior al pago del servicio es de: BS " << miCuenta.corriente << std::endl;
-                                } else if (tipoDeCuenta == 2) {
+                                } else if (miEntrada.tipDeCue == 2) {
                                     resta(miCuenta.ahorro, miEntrada.monto, miFactura.electricidad);
                                     std::cout << "**OPERACION EXITOSA**" <<std::endl;
                                     std::cout << std::endl;
@@ -192,8 +199,8 @@ int main() {
                                 }
                                 std::cout << std::endl;
                                 std::cout << "INGRESE CUALQUIER NUMERO PARA VOLVER AL MENU DE PAGOS DE SERVICIOS: ";
-                                std::cin >> opcionMenuServicioIf;
-                            } else if (opcionMenuServicio == 2) {
+                                std::cin >> miEntrada.opcMenSerIf;
+                            } else if (miEntrada.opcMenSer == 2) {
                                 std::cout << std::endl;
                                 std::cout << "**AGUA**" << std::endl;
                                 std::cout << std::endl;
@@ -205,12 +212,12 @@ int main() {
                                 std::cout << "#2 Cuenta ahorro" << std::endl;
                                 std::cout << std::endl;
                                 std::cout << "INGRESE LA OPCION DEL TIPO DE CUENTA PARA EL PAGO DE SERVICIO: ";
-                                std::cin >> tipoDeCuenta;
+                                std::cin >> miEntrada.tipDeCue;
                                 std::cout << std::endl;
                                 std::cout << "INGRESE MONTO: ";
                                 std::cin >> miEntrada.monto;
                                 std::cout << std::endl;
-                                if (tipoDeCuenta == 1) {
+                                if (miEntrada.tipDeCue == 1) {
                                     resta(miCuenta.corriente, miEntrada.monto, miFactura.agua);
                                     std::cout << "**OPERACION EXITOSA**" <<std::endl;
                                     std::cout << std::endl;
@@ -220,7 +227,7 @@ int main() {
                                     std::cout << std::endl;
                                     std::cout << "*El saldo actual de su cuenta corriente posterior al pago del servicio es de: BS " << miCuenta.corriente << std::endl;
                                     std::cout << std::endl;
-                                } else if (tipoDeCuenta == 2) {
+                                } else if (miEntrada.tipDeCue == 2) {
                                     resta(miCuenta.ahorro, miEntrada.monto, miFactura.agua);
                                     std::cout << "**OPERACION EXITOSA**" <<std::endl;
                                     std::cout << std::endl;
@@ -230,8 +237,8 @@ int main() {
                                 }
                                 std::cout << std::endl;
                                 std::cout << "INGRESE CUALQUIER NUMERO PARA VOLVER AL MENU DE PAGOS DE SERVICIOS: ";
-                                std::cin >> opcionMenuServicioIf;
-                            } else if (opcionMenuServicio == 3) {
+                                std::cin >> miEntrada.opcMenSerIf;
+                            } else if (miEntrada.opcMenSer == 3) {
                                 std::cout << std::endl;
                                 std::cout << "**INTERNET**" <<std::endl;
                                 std::cout << std::endl;
@@ -243,19 +250,19 @@ int main() {
                                 std::cout << "#2 Cuenta ahorro" <<std::endl;
                                 std::cout << std::endl;
                                 std::cout << "INGRESE EL NUMERO DEL TIPO DE CUENTA PARA EL PAGO DE SERVICIO: ";
-                                std::cin >> tipoDeCuenta;
+                                std::cin >> miEntrada.tipDeCue;
                                 std::cout << std::endl;
                                 std::cout << "INGRESE MONTO: ";
                                 std::cin >> miEntrada.monto;
                                 std::cout << std::endl;
-                                if (tipoDeCuenta == 1) {
+                                if (miEntrada.tipDeCue == 1) {
                                     resta(miCuenta.corriente, miEntrada.monto, miFactura.internet);
                                     std::cout << "**OPERACION EXITOSA**" <<std::endl;
                                     std::cout << std::endl;
                                     std::cout << "*El pago fue realizado exitosamente por un monto de: BS " << miEntrada.monto << " desde su cuenta corriente" << std::endl;
                                     std::cout << "*La deuda pendiente con respecto al servicio es de: BS " << miFactura.internet << std::endl;
                                     std::cout << "*El saldo actual de su cuenta corriente posterior al pago del servicio es de: BS " << miCuenta.corriente << std::endl;
-                                } else if (tipoDeCuenta == 2) {
+                                } else if (miEntrada.tipDeCue == 2) {
                                     resta(miCuenta.ahorro,  miEntrada.monto, miFactura.internet);
                                     std::cout << "**OPERACION EXITOSA**" <<std::endl;
                                     std::cout << std::endl;
@@ -265,7 +272,7 @@ int main() {
                                 }
                                 std::cout << std::endl;
                                 std::cout << "INGRESE CUALQUIER NUMERO PARA VOLVER AL MENU DE PAGOS DE SERVICIOS: ";
-                                std::cin >> opcionMenuServicioIf;
+                                std::cin >> miEntrada.opcMenSerIf;
                             }
                         }     
                         break;
@@ -276,34 +283,27 @@ int main() {
         }
 return 0;
 }
-bool verifInicioSesion(std::string usuarioCorrecto, int claveCorrecta){
-    std::string intentoUsuario;
-    int intentoClave;
+bool verifInicioSesion(entradas &entrada){
     for (int i = 0; i < 3; i++) {
         std::cout << "**INICIO DE SESION**" << std::endl;
         std::cout << std::endl;
         std::cout << "Introduzca su nombre de usuario: ";
-        std::cin >> intentoUsuario;
+        std::cin >> entrada.usuarioInput;
         std::cout << std::endl;
         std::cout << "Introduzca su clave: ";
-        std::cin >> intentoClave;
+        std::cin >> entrada.claveInput;
         std::cout << std::endl;
-        if ( intentoUsuario == usuarioCorrecto && intentoClave == claveCorrecta){
+        if (entrada.usuarioInput == entrada.usuarioCto && entrada.claveInput == entrada.claveCto){
             return true;
         }
-    std::cout << "Su usuario o clave son incorrectos, Intentos restantes: " << (2 - i) << std::endl;
-    std::cout << std::endl;
+        std::cout << "Su usuario o clave son incorrectos, Intentos restantes: " << (2 - i) << std::endl;
+        std::cout << std::endl;
     }
     return false;
 }
-void conversion(double &ctaCteBs, double &ctaAroBs, double &ctaCteDs, double &ctaAroDs, double tasa){
-    if (tasa == 0){
-        std::cout << "No se puede dividir por cero" << std::endl;
-    }
-    ctaCteDs = ctaCteBs / tasa;
-    ctaAroDs = ctaAroBs / tasa;
-    ctaCteBs = tasa * ctaCteDs;
-    ctaAroBs = tasa * ctaAroDs;
+void conversion(double ctaCteBs , double ctaAroBs, double &ctaCteDs, double &ctaAroDs, double tasa){
+        ctaCteDs = ctaCteBs / tasa;
+        ctaAroDs = ctaAroBs / tasa;
 }
 void transferencia (double &ctaDesc, double &ctaAum, double monto){
     ctaDesc = ctaDesc - monto;
